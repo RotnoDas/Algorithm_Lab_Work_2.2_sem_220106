4e4a0fa8631c66f5d264dcf9f386d6ac68097037
Rotno Chandra Das <147048298+RotnoDas@users.noreply.github.com>
2025-03-04 08:46:07 +0600

    BFS.cpp

--- BFS.cpp
+++ BFS.cpp
@@ -1,64 +1,75 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
 #define white 1
 #define gray 2
 #define black 3
+
 int node, edge;
 int color[100], parent[100], dis[100], graph[100][100];
+
 void bfs(int starting_node);
+
 int main()
 {
     cout << "Enter node and edge: ";
     cin >> node >> edge;
-    for(int i = 0; i < node; i++)
+
+    for (int i = 0; i < node; i++)
     {
-        for(int j = 0; j < node; j++)
+        for (int j = 0; j < node; j++)
         {
             graph[i][j] = 0;
         }
     }
-    cout << "Enter adjacency matrix: ";
+
+    cout << "Enter adjacency matrix (edges): ";
     int u, v;
-    for(int i = 0; i < edge; i++)
+    for (int i = 0; i < edge; i++)
     {
         cin >> u >> v;
         graph[u][v] = 1;
         graph[v][u] = 1;
     }
+
     int starting_node;
     cout << "Enter starting node: ";
     cin >> starting_node;
+
     bfs(starting_node);
+
     return 0;
 }
+
 void bfs(int starting_node)
 {
-    for(int i = 0; i < node; i++)
+    for (int i = 0; i < node; i++)
     {
         color[i] = white;
-        dis[i] = INT_MIN;
+        dis[i] = INT_MAX;
         parent[i] = -1;
     }
+
     dis[starting_node] = 0;
     parent[starting_node] = -1;
+    color[starting_node] = gray;
     queue<int> q;
     q.push(starting_node);
-    while(!q.empty())
+
+    while (!q.empty())
     {
         int x = q.front();
         q.pop();
-        color[x] = gray;
         cout << x << ' ';
-        for(int i = 0; i < node; i++)
+
+        for (int i = 0; i < node; i++)
         {
-            if(graph[x][i] == 1)
+            if (graph[x][i] == 1 && color[i] == white)
             {
-                if(color[i] == white)
-                {
-                    q.push(i);
-                    parent[i] = x;
-                    dis[i] = dis[x] + 1;
-                }
+                q.push(i);
+                color[i] = gray;
+                parent[i] = x;
+                dis[i] = dis[x] + 1;
             }
         }
         color[x] = black;
